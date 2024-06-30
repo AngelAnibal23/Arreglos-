a700d9400db9f988308d9105dc8ea7859b90c9fb
Guts <aamamanica@unjbg.edu.pe>
2024-06-30 06:01:41 -0500

    Subiendo el ejercicio 11 de arreglos

---
+++ Ejercicio11-arreglos.cpp
@@ -0,0 +1,30 @@
+/*
+Ejercicio 11: 
+Calcular la suma de elementos positivos y negativos en un arreglo.
+*/
+
+#include <iostream>
+using namespace std; 
+int main(){
+	int suma_p=0, suma_n=0, n, numeros[30];
+	
+	cout<<"Digite la cantidad de elementos: ";
+	cin>>n; 
+	
+	for(int i=0; i<n; i++){
+		cout<<"Digite el elemento: "; 
+		cin>>numeros[i]; 
+		
+		if(numeros[i]>0){
+			suma_p+=numeros[i];
+		}
+		else if(numeros[i]<0){
+			suma_n+=numeros[i]; 
+		}
+	} 
+	
+	cout<<"\nLa suma de los numeros positivos es: "<<suma_p<<endl; 
+	cout<<"La suma de los numeros negativos es: "<<suma_n<<endl; 
+	
+	return 0; 
+}
