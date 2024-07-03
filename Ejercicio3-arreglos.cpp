3e0385bde04e9f089788ea61bf1f6c7fcb19cfbc
Guts <aamamanica@unjbg.edu.pe>
2024-07-02 20:51:17 -0500

    Subiendo el ejercicio 3 de arreglos

---
+++ Ejercicio3-arreglos.cpp
@@ -0,0 +1,29 @@
+/*
+Ejercicio 3: 
+Contar la ocurrencia de una letra específica en una cadena de entrada
+*/
+
+#include <iostream>
+#include <string.h>
+#include <cstring>
+using namespace std; 
+int main(){
+	char letra, palabra[100]; 
+	int cont=0; 
+	
+	cout<<"Digite su cadena: "; 
+	cin.getline(palabra,100); 
+	cout<<endl; 
+	cout<<"Que letra le gustaria comparar: "; 
+	cin>>letra; 
+	
+	for(int i=0; palabra[i] != '\0'; i++){
+		if(letra==palabra[i]){
+			cont++; 
+		}
+	}
+	cout<<endl; 
+	cout<<"La letra elegida: "<<letra<<". Se repite : "<<cont<<" veces. "<<endl; 
+	
+	return 0; 
+}
